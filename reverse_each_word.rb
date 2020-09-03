70b0cdefc77d403279c8f1be221877c9d979712f
Rodney Smith II <r_smith3@hotmail.com>
2020-09-03 23:06:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(string)
   new_array = []
-  string.split(" ").each {|word| word.reverse}.join(" ")
+  string.split(" ").each {|word| new_array << word.reverse}.join(" ")
+  new_array
 end
